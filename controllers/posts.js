0470f8d185700d8f061ab60ea4463167d6dcfb0c
alessandra martera <ale.martera@gmail.com>
2023-11-13 15:39:45 +0100

    clean up show in posts.js

--- controllers/posts.js
+++ controllers/posts.js
@@ -38,7 +38,6 @@ module.exports.show = function(req, res) {
 // creo una nuova costante in cui mettere il json che mi serve
 // prendo l'array principale e vedo se è presente uno slug ugale a quello che sto cercando
 // quando lo trovo lo metto dentro alla costante appena creata
-
    const post = posts.find(post => post.slug === postSlug);
 
 // controllo se effettivamente estiste il json che ho appena cercato 
@@ -46,25 +45,5 @@ module.exports.show = function(req, res) {
     res.json(post);
    } else{
     res.status(404).send(`<h1 style="text-align: center;"> il post relativo a ${postSlug} non è stato trovato </h1>`)
-   }
-  
-    // res.send('<h1> Dettagli del posto relativo a: ' + post.slug + '</h1>');
-
-    
+   }    
 }
-
-// function findOrFail(req, res) {
-//     // recupero l'id dalla richiesta
-//     const slugPost = req.params.slug;
-   
-//     // recupero la pizza dal menu
-//     const post = menu.find((post) => post.slug == slugPost);
-  
-//     // Nel caso in cui non sia stata trovata la post ritorno un 404
-//     if (!post) {
-//       res.status(404).send(`post con id ${slugPost} non trovata`);
-//       return; // interrompo l'esecuzione della funzione
-//     }
-  
-//     return post;
-//   }
